4c228543e195bd241664d3b183e017de3b89d896
Susan Ahmed <susanahmed617@gmail.com>
2023-02-28 20:45:46 -0500

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__ (self, title, page_count=0):
+        self.title = title
+        self._page_count = page_count
+
+    def get_author(self):
+        return self.author
+    def set_author(self, author):
+        self.author = author
+    
+    def get_page_count(self):
+        return self._page_count
+    def set_page_count(self, page_count):
+        if type (page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    page_count = property(get_page_count, set_page_count)
+
+    def get_genre(self):
+        return self.genre
+    def set_genre(self, genre):
+        self.genre = genre
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
